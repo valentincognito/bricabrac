b49966623c8178c48b67f9bd10798d27babf4ac3
valentincognito <valentin.vannay@gmail.com>
2017-04-19 18:20:07 +0900

    Add paddle and ball

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+	void OnTriggerEnter2D(Collider2D trigger){
+		print("Trigger");
+		levelManager.LoadLevel("Win");
+	}
+}

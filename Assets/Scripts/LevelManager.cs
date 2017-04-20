30ab3490ebfea67bf91b36a9d138775a8a39ec83
valentincognito <valentin.vannay@gmail.com>
2017-04-20 15:48:15 +0900

    Fix prefabs

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name){
-		Debug.Log ("Level load requested for: "+name);
 		Application.LoadLevel(name);
 	}
 
--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class LoseCollider : MonoBehaviour {
 
-	public LevelManager levelManager;
+	private LevelManager levelManager;
+
+	void Start(){
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
 	void OnTriggerEnter2D(Collider2D trigger){
-		print("Trigger");
 		levelManager.LoadLevel("Win");
 	}
 }

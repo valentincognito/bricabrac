ca2153d967c4636b43fbaef37ef8d47f765586be
valentincognito <valentin.vannay@gmail.com>
2017-04-20 13:45:00 +0900

    Start game on click

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 paddlePos = new Vector3 (this.transform.position.x, this.transform.position.y, 0f);
+
+		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+
+		paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0f, 15f);
+		this.transform.position = paddlePos;
+	}
+}

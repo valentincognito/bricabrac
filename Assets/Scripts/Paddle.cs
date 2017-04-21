153def887d125cb3c9ef8e7231f956d6359a5398
valentincognito <valentin.vannay@gmail.com>
2017-04-21 12:40:56 +0900

    Update sprites

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,18 +3,31 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public bool autoPlay = false;
+	private Ball ball;
+
+	void Start(){
+		ball = GameObject.FindObjectOfType<Ball>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(!autoPlay){
+			MoveWithMouse();	
+		}else{
+			AutoPlay();
+		}
+	}
+	void MoveWithMouse(){
 		Vector3 paddlePos = new Vector3 (this.transform.position.x, this.transform.position.y, 0f);
-
 		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
-
 		paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0f, 15f);
+		this.transform.position = paddlePos;
+	}
+	void AutoPlay(){
+		Vector3 paddlePos = new Vector3 (this.transform.position.x, this.transform.position.y, 0f);
+		Vector3 ballPos = ball.transform.position;
+		paddlePos.x = Mathf.Clamp(ballPos.x, 0f, 15f);
 		this.transform.position = paddlePos;
 	}
 }
